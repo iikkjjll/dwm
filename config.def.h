4b5de930f30bf672d1d2f70c6d7573b1341841fb
iikkjjll <iikkjjll@gitee.com>
2023-07-23 09:09:34 +0800

    modified the border width and color.

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -13,17 +13,19 @@ static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
+static const char col_border[]        = "#282a36";
 static const char col_hui[]        = "#826858";
-static const char col_purple[]        = "#E800E8";
+static const char col_purple[]        = "#f300ea";
 static const char col_yellow[]        = "#FFE600";
+static const char col_blue[]        = "#39aaf7";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_hui },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_yellow  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_border },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_purple  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
