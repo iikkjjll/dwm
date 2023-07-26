be2f97a8213bff2031696a4c70af040e6a8dd3f4
iikkjjll <iikkjjll@gitee.com>
2023-07-26 10:01:53 +0800

    patched: patches/dwm-alpha-systray-6.3.diff

--- config.def.h
+++ config.def.h
@@ -6,6 +6,10 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;   /* 0 means no systray */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10",
 										"WenQuanYi WenQuanYi Bitmap Song:size=10:type=Regular:antialias=true:autohint=true",
 										"Symbols Nerd Font:pixelsize=14:type=Regular:antialias=true:autohint=true" };
