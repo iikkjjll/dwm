3b4412ac478fd2156f3c4f72cd68f3f1938d1a4e
iikkjjll <iikkjjll@gitee.com>
2023-08-27 20:28:56 +0800

    modified the Xmodmap for mechanical keyboard.

--- scripts/fix_xmodmap_xset.sh
+++ scripts/fix_xmodmap_xset.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 xset r rate 160 20
-[[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
+# [[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
+
+# 这是外接键盘的映射
+[[ -f ~/.Xmodmaprc ]] && xmodmap ~/.Xmodmaprc
+
+# 插入外接键盘后，原本的 id=11 变成了 15
+xinput set-prop 15 183 0 
